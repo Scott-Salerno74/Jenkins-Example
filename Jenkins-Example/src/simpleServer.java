b17cbf3ba8b3487b088ca0df83b107fe10707c73
Scott-Salerno74 <sws001@lvc.edu>
2019-03-15 09:48:48 -0400

    Not Ready yet, must complete server Side

--- Jenkins-Example/src/simpleServer.java
+++ Jenkins-Example/src/simpleServer.java
@@ -1,22 +1,47 @@
 import java.net.*;
 import java.io.*;
 public class simpleServer{
- public static void main(String[] args) throws IOException{
+ public static void main(String[] args) throws IOException {
+     int numRequests = 0;
      ServerSocket serverSocket = null;
-     try{
-         serverSocket = new ServerSocket(1234);
-     } catch (IOException e){
+     try {
+         serverSocket = new ServerSocket(8081);
+     } catch (IOException e) {
          System.err.println("Could not listen on port: 1234");
          System.exit(1);
      }
 
      Socket clientSocket = null;
-     try{
+     try {
          clientSocket = serverSocket.accept();
-     } catch(IOException e){
+     } catch (IOException e) {
          System.err.println("Was not Accepted!");
          System.exit(1);
      }
+
+     try {
+         BufferedReader brIn = new BufferedReader(new InputStreamReader(
+                 clientSocket.getInputStream()
+         ));
+         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
+
+
+     } catch (IOException e){
+           System.out.println(" Failure, We Suck Again!");
+           System.exit(1);
+
+     }
+
+  while(true){
+      try{
+          String line = brIn.readLine
+      }
+  }
+
+
+
+
+
 
 
  }
