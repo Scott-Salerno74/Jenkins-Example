e5190bb2f0bef68e98912c6651e20dc561350f23
Scott-Salerno74 <sws001@lvc.edu>
2019-03-18 09:50:51 -0400

    Server up, just one problem in Simple Handler

--- Jenkins-Example/src/simpleHandler.java
+++ Jenkins-Example/src/simpleHandler.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 public class simpleHandler extends AbstractHandler {
 
     protected int totalNumberOfRequests = 0;
--- Jenkins-Example/src/simpleServer.java
+++ Jenkins-Example/src/simpleServer.java
@@ -1,12 +1,17 @@
+import org.eclipse.jetty.server.Server;
+
 import java.io.*;
 import java.net.Socket;
 import java.net.ServerSocket;
 public class simpleServer {
     public static void main(String[] args) throws IOException {
         int numRequests = 0;
-        final ServerSocket server = new ServerSocket(8080);
+        final ServerSocket server = new ServerSocket(8081);
         System.out.println("Listening for connection on port 8080");
-
+        Server server1 = new Server(8080);
+        server1.setHandler(simpleHandler);
+        server1.start();
+        server1.join();
 
         while (true){
             final Socket clientSocket = server.accept();
